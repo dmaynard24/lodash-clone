a041c7398e00be98c7bc12259865d85ca5649ed3
Dave Maynard <dmaynard@sagepath.com>
2020-03-09 16:36:54 +0100

    added an additional test case to sortedUniq to account for strings

--- javascript/sortedUniq/sortedUniq.test.js
+++ javascript/sortedUniq/sortedUniq.test.js
@@ -7,3 +7,7 @@ test(`expects that when the sorted array [1, 1, 2, 3, 3, 3, 5, 8, 8] is stripped
 test(`expects that when the sorted array [1, 1, 2, 3, 3, 3, 5, 8] is stripped of all non-unique elements that it will return [1, 2, 3, 5, 8]`, () => {
   expect(sortedUniq([1, 1, 2, 3, 3, 3, 5, 8])).toStrictEqual([1, 2, 3, 5, 8]);
 });
+
+test(`expects that when the sorted array ['a', 'a', 'b', 'd', 'd', 'd', 'g'] is stripped of all non-unique elements that it will return ['a', 'b', 'd', 'g']`, () => {
+  expect(sortedUniq([`a`, `a`, `b`, `d`, `d`, `d`, `g`])).toStrictEqual([`a`, `b`, `d`, `g`]);
+});
